38d22e867d84e95bff8c7418f9bfa983dc8395ce
JustAPotota <justapotota.dev@gmail.com>
2024-12-17 00:15:03 -0600

    Better example

--- main/marble.go
+++ main/marble.go
@@ -1,70 +1,44 @@
+components {
+  id: "marble"
+  component: "/main/marble.script"
+}
 embedded_components {
   id: "model"
   type: "model"
   data: "mesh: \"/builtins/assets/meshes/sphere.dae\"\n"
-  "material: \"/main/model.material\"\n"
-  "textures: \"/main/brown.png\"\n"
-  "skeleton: \"\"\n"
-  "animations: \"\"\n"
-  "default_animation: \"\"\n"
   "name: \"unnamed\"\n"
+  "materials {\n"
+  "  name: \"default\"\n"
+  "  material: \"/main/model.material\"\n"
+  "  textures {\n"
+  "    sampler: \"tex0\"\n"
+  "    texture: \"/main/assets/marble.png\"\n"
+  "  }\n"
+  "}\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  data: "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
   "mass: 0.1\n"
   "friction: 0.5\n"
   "restitution: 1.0\n"
   "group: \"marbles\"\n"
   "mask: \"world\"\n"
   "mask: \"marbles\"\n"
+  "mask: \"reset\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
-  "      z: 0.0\n"
-  "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
   "    count: 1\n"
   "  }\n"
   "  data: 0.5\n"
   "}\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
-  "bullet: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
